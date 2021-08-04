8f4fb69b10f04cf77f11191cc69fa99a20f82610
AlinaDiaz21 <88359468+AlinaDiaz21@users.noreply.github.com>
2021-08-03 20:05:12 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 30 18:37:34 2021
+
+@author: Usuario
+"""
+
+#INPUT
+
+nombre=input("Por favor ingrese su nombre: ")
+
+
+apellido=input("Ahora ingrese su apellido: ")
+
+
+ciudad=input("Por favor ingrese la ciudad en que vive: ")
+
+
+edad=input("Por favor ingrese su edad: ")
+
+
+space=" "
+
+
+print("Hola", nombre,space, apellido)
+print("Se necesita la verificación de sus datos personales")
+
+print("Nombre:", nombre,space,apellido)
+print("Ciudad:", ciudad)
+print("Edad:", edad)
---
+++ sol_02_list-dcts.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 30 18:29:32 2021
+
+@author: Usuario
+"""
+
