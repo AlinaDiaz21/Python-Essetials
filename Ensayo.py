7e608f3215f563b5afbfed4eeb3872e95b713226
AlinaDiaz21 <88359468+AlinaDiaz21@users.noreply.github.com>
2021-08-07 11:25:59 -0500

    Add files via upload

---
+++ Ensayo.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  7 11:16:27 2021
+
+@author: Usuario
+"""
+
+#ENSAYO
+
+"""
+GitHub, Stack overflow y DevNet son COPs, estos diferentes espacios/plataformas de colaboración 
+buscan contribuir de distintas maneras a la elaboración de un código de programación.. En GitHub 
+se alberga una gran cantidad de repositorios que contienen proyectos de código abierto, es decir, 
+brinda la posibilidad de revisar los códigos creados por otras personas, otorga la posibilidad de 
+aprender de estos códigos y así poder solucionar los problemas de desarrollo propios. Además, 
+permite guardar un repositorio con el código que se trabaje de manera individual o colaborativa. 
+GitHub no está diseñado exclusivamente para los desarrolladores, también es de mucha ayuda 
+para otras ramas profesionales, ya que pueden usarlo cuando trabajan con un equipo de más 
+personas al mismo tiempo. Stack overflow en cambio es una plataforma de preguntas y respuestas 
+sobre diferentes tópicos del desarrollo/uso de software. A diferencia de GitHub, se basa en la 
+resolución de problemas de manera colaborativa, por lo que podría ser confundido con una 
+especie de foro, sin embargo, es bastante diferente puesto que no cualquiera brinda su opinión 
+(relevante/irrelevante) y tampoco se llevan a cabo debates o socializaciones en este medio. Las 
+soluciones que se dan se posicionan en base a la utilidad evaluada por la comunidad, misma que 
+debe ser brindada para la solución de preguntas de manera objetiva, y a su vez, se acepta o 
+rechaza por la persona que preguntó inicialmente. En Stack overflow, lo que brinda una mayor 
+“Reputación” son las respuestas acertadas y mejor calificadas, puesto que las altas calificaciones a 
+las respuestas brindadas, otorgan una mayor reputación y desbloquea privilegios. Entre los 
+privilegios que se pueden conseguir en stack overflow, se encuentra el acceso a estadísticas, editar 
+preguntas y respuestas, etc.,. Finalmente, DevNet es un espacio de aprendizaje para la 
+construcción, empleo de las diferentes herramientas y la integración de estas con Cisco. Mediante 
+DevNet, se satisfacen inquietudes de desarrollo de los usuarios, por parte del equipo de Cisco. 
+Como se mencionó con anterioridad, las ventajas a destacar de estos espacios (GitHub, Stack 
+overflow y DevNet) son parte de las ventajas de los COPs, Una especie de comunidades de 
+práctica/aprendizaje de diferentes lenguajes de programación. En base a la funcionalidad de las 
+COPs que se abordaron, se puede destacar la colaboración de los usuarios para la elaboración de 
+código, solución de problemas y búsqueda de nuev"as herramientas, así como el empleo de estas 
+para mejorar el aprendizaje de programación."""
+
+#FUENTES
+
+"""
+https://github.com/
+
+https://es.stackoverflow.com/
+
+https://developer.cisco.com/
+
+"""
+
+
+
+                                                                              
