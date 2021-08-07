66795a489ad8d020e97066db4eca99432cf6f3ee
AlinaDiaz21 <88359468+AlinaDiaz21@users.noreply.github.com>
2021-08-07 18:33:25 -0500

    Add files via upload

---
+++ Laboratorio 1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  7 17:31:22 2021
+
+@author: Alina Díaz
+"""
+x=float(input("Ingrese el valor de x:"))
+y =(3*(x**3) - 2*(x**2) + 3*(x) -1)
+print("El resultado es:",y)
+
+
+
+
+
+
+
+
+
+
+
